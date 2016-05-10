84eae760c0d24d5d6868dadebd401e32379151af
Sndevil <rezaiemohammadreza@gmail.com>
2016-05-10 09:32:41 +0430

    version 1.3, Average added some minor fixes for crashing

--- GPSNavigator/GPSNavigator/Grapher.cs
+++ GPSNavigator/GPSNavigator/Grapher.cs
@@ -184,7 +184,7 @@ namespace GPSNavigator
                                 avg = 0;
                             }
 
-                        }
+                        }                        
 
                     }
 
